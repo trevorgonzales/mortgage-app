a2405ce1425c93367eed7b81c6fa768615c953a5
Trevor_Gonzales <54246714+trevorgonzales@users.noreply.github.com>
2021-08-18 21:45:37 -0700

    Next introduced

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import { connectToDatabase } from '../lib/mongodb'
+
+export default function Home({ isConnected }) {
+  return (
+    <div>
+      <h1>Mortgage App</h1>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export async function getServerSideProps(context) {
+  const { client } = await connectToDatabase()
+
+  const isConnected = await client.isConnected()
+
+  return {
+    props: { isConnected },
+  }
+}
